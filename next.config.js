124323b737ae7218ce4463cbfd9aa3c689d275ae
Ryosuke <ryosuke.san.hana@gmail.com>
2022-02-03 18:00:42 -0800

    :sparkles: v2 working - maybe?

--- next.config.js
+++ next.config.js
@@ -1,35 +1,6 @@
-// const withMDX = require("@next/mdx")({
-//   extension: /\.mdx?$/,
-// });
-const remarkGfm = require("remark-gfm");
-// import remarkGfm from "remark-gfm";
-module.exports = {
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx"],
-
-  // Required for Remark GFM (ESM Module)
-  experimental: { esmExternals: true },
-
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mdx/,
-      use: [
-        {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-        {
-          loader: "@mdx-js/loader",
-          /** @type {import('@mdx-js/loader').Options} */
-          options: {
-            providerImportSource: "@mdx-js/react",
-            remarkPlugins: [remarkGfm],
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
-};
+});
